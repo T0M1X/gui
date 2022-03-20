7ecd1fc7054b78fe02333d079231c184fcf942c9
Qterion <60145661+Qterion@users.noreply.github.com>
2022-03-20 23:26:16 +0600

    clean version

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import React, { Component, useEffect, useState } from "react";
 import Weather from './components/weather';
 import Homepage from './components/weather/homepage';
 import { Dimmer, Loader } from 'semantic-ui-react';
-import { geolocated, geoPropTypes} from "react-geolocated";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import useGeo from './hooks/useGeo';
 export default function App() {
 
   const [data, setData] = useState([]);
   const location=useGeo();
-  App.propTypes = Object.assign({}, App.propTypes, geolocated);
   useEffect(() => {
     const fetchData = async () => {
       if(location.loaded){
@@ -30,7 +28,6 @@ export default function App() {
         <Route path='/' element={<Homepage />} />
         <Route path='/weather' element={
           <div className="container">
-            <h1 style={{color:'black'}}>{location.loaded?  JSON.stringify(location): "ggege"}</h1>
           {(typeof data.main != 'undefined') ? (
             <Weather weatherData={data}/>
           ): (
