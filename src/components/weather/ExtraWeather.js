1114017a713f56bc9a070ab10de6afcd6d4a1e99
C3-H4 <71638696+C3-H4@users.noreply.github.com>
2022-03-23 13:32:40 +0000

    TEMP NOT COMPLETED
    need more work - save progress

--- src/components/weather/ExtraWeather.js
+++ src/components/weather/ExtraWeather.js
@@ -19,6 +19,69 @@ function ExtraWeather({ weatherData }) {
             <img className="refresh-button" src={`images/refresh.png`}></img>
           </Button>
         </div>
+
+        <h1 className="title-Weekly">
+          Weekly View
+        </h1>
+
+        <table>
+          <tr>
+            <th>
+              Day
+            </th>
+            <th>
+              Temp
+            </th>
+            <th>
+              Wind-Spd
+            </th>
+            <th>
+              Humidity
+            </th>
+            <th>
+              Sunrise - Sunset
+            </th>
+
+          </tr>
+          <tr>
+            <td>
+              Monday
+            </td>
+            <td>
+              <img src="images/01d.png" class="IconTD"></img>
+            </td>
+            <td>
+              2.06 mph
+            </td>
+            <td>
+              34%
+            </td>
+            <td>
+              5:56:26 - 6:17:17
+            </td>
+
+          </tr>
+          <tr>
+          <td>
+              Tuesday
+            </td>
+            <td>
+              10c
+            </td>
+            <td>
+              3.62 mph
+            </td>
+            <td>
+              15%
+            </td>
+            <td>
+              5:56:26 - 6:17:17
+            </td>
+
+          </tr>
+        </table>
+
+
       </div>
     </div>
   );
